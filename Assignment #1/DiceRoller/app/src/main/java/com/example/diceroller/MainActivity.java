b2e3ae3a60daea6cf173b4d7d9aa6fd36cc38f7d
daniiar <danraiymkulov@gmail.com>
2019-06-14 14:36:12 +0600

    Added landscape mode support for the Dice Roll app

--- Assignment #1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ Assignment #1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.diceroller;
 
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,53 +12,35 @@ public class MainActivity extends AppCompatActivity {
     final Random random = new Random();
     int dice1number, dice2number = 1;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState != null) {
-            setDiceImageResource(savedInstanceState.getInt("dice1number"), savedInstanceState.getInt("dice2number"));;
+            dice1number = savedInstanceState.getInt("dice1number");
+            dice2number = savedInstanceState.getInt("dice2number");
+            setDiceImageResource(dice1number, dice2number);
         }
+        setDiceImageResource(dice1number, dice2number);
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setContentView(R.layout.activity_main_landscape);
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-            setContentView(R.layout.activity_main);
-        }
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outstate) {
-        super.onSaveInstanceState(outstate);
-        outstate.putInt("dice1number", dice1number);
-        outstate.putInt("dice2number", dice2number);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("dice1number", dice1number);
+        outState.putInt("dice2number", dice2number);
     }
 
     public void rollDice(View view) {
+        dice1number = random.nextInt(6) + 1;
+        dice2number = random.nextInt(6) + 1;
         setDiceImageResource(dice1number, dice2number);
     }
-    private int createRandomNumber1() {
-        dice1number = random.nextInt(6) + 1;
-        return dice1number;
-    }
 
-    private int createRandomNumber2() {
-        dice2number = random.nextInt(6) + 1;
-        return dice2number;
-    }
-
-    public void setDiceImageResource(int dice1number, int dice2number) {
+    private void setDiceImageResource(int diceOneNumber, int diceTwoNumber) {
         ImageView dice1ImageView = findViewById(R.id.dice1_imageView);
         ImageView dice2ImageView = findViewById(R.id.dice2_imageView);
-        switch (createRandomNumber1()) {
+        switch (diceOneNumber) {
             case 1:
                 dice1ImageView.setImageResource(R.drawable.dice_1);
                 break;
@@ -80,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 dice1ImageView.setImageResource(R.drawable.dice_6);
                 break;
         }
-        switch (createRandomNumber2()) {
+        switch (diceTwoNumber) {
             case 1:
                 dice2ImageView.setImageResource(R.drawable.dice_1);
                 break;
